16d8256f3f3e5b9e4dee3adb1db225fafe10b4fd
Brandin Crockett <brandincrockett@gmail.com>
2023-03-31 13:01:56 -0700

    Completed Assignment 005

--- src/TempConversion.java
+++ src/TempConversion.java
@@ -23,6 +23,10 @@ public class TempConversion {
         return kelvin - 273.15;
     }
 
+    public static double convertK2F(double kelvin) {
+        return convertC2F(convertK2C(kelvin));
+    }
+
     public static double getTemp(String unit) {
         System.out.printf("%-40s : ", String.format("Please enter °%s temperature to convert", unit));
         return Double.parseDouble(input.nextLine());
@@ -38,7 +42,46 @@ public class TempConversion {
 
     public static void main(String[] args) {
         while(true) {
+            System.out.println("Enter the unit to convert from:");
+            String fromUnit = getUnitChoice().toUpperCase();
+            if (fromUnit.equals("Q")) {
+                break;
+            }
+
+            System.out.println("Enter the unit to convert to:");
+            String toUnit = getUnitChoice().toUpperCase();
+            if (toUnit.equals("Q")) {
+                break;
+            }
+
+            double inputValue = getTemp(fromUnit);
+            double convertedValue;
+
+            switch (fromUnit + toUnit) {
+                case "CF":
+                    convertedValue = convertC2F(inputValue);
+                    break;
+                case "CK":
+                    convertedValue = convertC2K(inputValue);
+                    break;
+                case "FC":
+                    convertedValue = convertF2C(inputValue);
+                    break;
+                case "FK":
+                    convertedValue = convertF2K(inputValue);
+                    break;
+                case "KC":
+                    convertedValue = convertK2C(inputValue);
+                    break;
+                case "KF":
+                    convertedValue = convertK2F(inputValue);
+                    break;
+                default:
+                    System.out.println("Invalid conversion. Please try again.");
+                    continue;
+            }
+            System.out.printf("Result: %.6f°%s is %.6f°%s%n", inputValue, fromUnit, convertedValue, toUnit);
+        }
 
         }
     }
-}
